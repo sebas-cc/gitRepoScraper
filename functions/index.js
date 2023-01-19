bfb5c5dd158da7539e61d37bbf75771e92f4eed1
sebas-cc <csebas459@gmail.com>
2023-01-18 19:01:33 -0500

    Netlify integration

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,21 @@
-const PORT = process.env.PORT || 8000
+const PORT = 8000
 
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { Router } = require('express')
 const express = require('express')
+const serverless = require('serverless-http')
 
 const app = express()
+const router = express.Router()
 
-app.get('/:userName', (req, res) => {
+router.get('/', (req, res) => {
+    res.json({
+        Welcome: "to page"
+    })
+})
+
+router.get('/:userName', (req, res) => {
     const user = req.params.userName
     axios(`https://github.com/${user}?tab=repositories`).then(response => {
         const html = response.data
@@ -24,5 +33,8 @@ app.get('/:userName', (req, res) => {
         res.json(repoList)
     }).catch(err => console.log(err))
 })
+
+app.use('/', router)
+module.exports.handler = serverless(app)
 
 app.listen(PORT, () => console.log(`Running on PORT: ${PORT}`))
